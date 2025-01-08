9240f216fa12e6286f50e6e67ca364db30c9c669
rawasaditya <rawasaditya@gmail.com>
2025-01-09 02:04:13 +0530

    update

--- server/src/app.js
+++ server/src/app.js
@@ -7,11 +7,10 @@ const { connectDb } = require("./config/database");
 connectDb()
   .then(() => {
     console.log("Database connected");
+    app.listen(process.env.PORT, async () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
   })
   .catch((err) => {
     console.log("Error connecting to database", err);
   });
-
-app.listen(3000, async () => {
-  console.log("Server has started");
-});
